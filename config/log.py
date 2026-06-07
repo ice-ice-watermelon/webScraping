9648c12bfddaf410f3e27e5ca44130b2abd98071
ice-ice-watermelon <v5745552377@163.com>
2026-06-07 17:40:27 +0800

    add console log

--- config/log.py
+++ config/log.py
@@ -1,13 +1,21 @@
 import logging
 
+
 def log_init():
     """
     初始化log日志
     """
+    log_format = logging.Formatter('%(asctime)s - [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s')
+
+    #log_file
+    file_handler = logging.FileHandler('log.log', mode='w', encoding='utf-8')
+    file_handler.setFormatter(log_format)
+
+    #log_console
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+
     logging.basicConfig(
         level=logging.INFO,
-        filename='log.log',
-        filemode='w',
-        format='%(asctime)s - [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s',
-        encoding='utf-8'
+        handlers=[file_handler, console_handler]
     )
